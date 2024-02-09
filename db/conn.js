b9e63b8d19fe3a8e9338aa06d62388d8dddb8939
han18 <han.fadah@gmail.com>
2024-02-09 15:19:09 -0500

    connecting to mongodb

--- db/conn.js
+++ db/conn.js
@@ -7,7 +7,7 @@ import mongoose from "mongoose";
 export async function conn() {
   try {
     await mongoose.connect(process.env.ATLAS_URI);
-    console.log("Connected to MongoDB");
+    console.log("");
   } catch (error) {
     console.log(error.message);
   }
