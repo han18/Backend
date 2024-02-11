0e23ed1e7d761eadf69426faf730a6ca778af80e
han18 <han.fadah@gmail.com>
2024-02-11 11:56:35 -0500

    create the post for new comments

--- routes/comment.js
+++ routes/comment.js
@@ -1,2 +1,15 @@
 import { Router } from "express";
 import Comment from "../models/comments.js";
+
+const router = new Router();
+
+// this creates a new comment
+router.post("/", async (req, res) => {
+  try {
+    const comment = new Comment(req.body);
+    await comment.save();
+    res.status(201).json(comment);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
