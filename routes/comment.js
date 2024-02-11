4885f0b005369ebcb8de0c6b88607dcb901f4c30
han18 <han.fadah@gmail.com>
2024-02-11 11:59:25 -0500

    creating the catch

--- routes/comment.js
+++ routes/comment.js
@@ -9,7 +9,5 @@ router.post("/", async (req, res) => {
     const comment = new Comment(req.body);
     await comment.save();
     res.status(201).json(comment);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  
 });
